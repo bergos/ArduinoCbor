b23efd1a8b0061fce4dbdfd6467939b67d0199c1
bergi <bergi@axolotlfarm.org>
2016-11-24 00:26:49 +0100

    initial commit

---
+++ src/CborObject.h
@@ -0,0 +1,27 @@
+#ifndef CBOR_OBJECT_H_
+#define CBOR_OBJECT_H_
+
+#include "ArduinoCbor.h"
+
+class CborObject {
+ friend class CborVariant;
+
+ public:
+  CborObject(CborBuffer& buffer, cn_cbor* raw=0);
+
+  CborVariant get(const char* key);
+
+  void set(const char* key, CborVariant value);
+  void set(const char* key, const char* value);
+  void set(const char* key, CBOR_INT_T value);
+  void set(const char* key, CborObject value);
+  void set(const char* key, CborArray value);
+
+  size_t encode(uint8_t* data, size_t size);
+
+ protected:
+  CborBuffer& buffer;
+  cn_cbor* raw;
+};
+
+#endif // CBOR_OBJECT_H_
